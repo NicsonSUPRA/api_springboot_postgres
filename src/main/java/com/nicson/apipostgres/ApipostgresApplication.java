4d278abf68402b3da010f31c8d5bf28d3ad769e7
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-08-15 09:20:15 -0300

    my commit

---
+++ src/main/java/com/nicson/apipostgres/ApipostgresApplication.java
@@ -0,0 +1,13 @@
+package com.nicson.apipostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipostgresApplication.class, args);
+	}
+
+}
