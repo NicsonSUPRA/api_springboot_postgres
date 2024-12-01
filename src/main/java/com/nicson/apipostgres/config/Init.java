f8af55db70ee29826055d8e70887378152aed3ba
NicsonSUPRA <nicsoncc1234@gmail.com>
2024-12-01 14:28:45 -0300

    merge

--- src/main/java/com/nicson/apipostgres/config/Init.java
+++ src/main/java/com/nicson/apipostgres/config/Init.java
@@ -11,12 +11,14 @@ import com.nicson.apipostgres.models.Category;
 import com.nicson.apipostgres.models.Order;
 import com.nicson.apipostgres.models.OrderItem;
 import com.nicson.apipostgres.models.Product;
+import com.nicson.apipostgres.models.Role;
 import com.nicson.apipostgres.models.User;
 import com.nicson.apipostgres.models.enums.OrderStatus;
 import com.nicson.apipostgres.repositories.CategoryRepository;
 import com.nicson.apipostgres.repositories.OrderItemRepository;
 import com.nicson.apipostgres.repositories.OrderRepository;
 import com.nicson.apipostgres.repositories.ProductRepository;
+import com.nicson.apipostgres.repositories.RoleRepository;
 import com.nicson.apipostgres.repositories.UserRepository;
 
 @Configuration
@@ -37,10 +39,19 @@ public class Init implements CommandLineRunner {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Role r1 = new Role("ADMIN");
+        Role r2 = new Role("USER");
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        u1.setRoles(Arrays.asList(r1));
+        u2.setRoles(Arrays.asList(r1, r2));
 
         Order o1 = new Order(0, new Date(), OrderStatus.PAID, u1);
         Order o2 = new Order(0, new Date(), OrderStatus.WAITING_PAYMENT, u2);
@@ -72,6 +83,9 @@ public class Init implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p4, p5));
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+        roleRepository.saveAll(Arrays.asList(r1, r2));
+
+        System.out.println(u2.getRoles());
     }
 
 }
